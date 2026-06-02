2f4b93199e12b64e468b89bf479e04ef33230ffa
coderaiser <coderaiser@cloudcmd.io>
2026-06-02 12:40:46 +0000

    chore: actions

--- .madrun.js
+++ .madrun.js
@@ -3,6 +3,7 @@ import {run} from 'madrun';
 export default {
     'test': () => 'tape test/*.js',
     'coverage': () => 'c8 npm test',
+    'report': () => 'c8 report --reporter=lcov',
     'lint': () => 'putout .',
     'fix:lint': () => run('lint', '--fix'),
 	'postfix:lint': () => 'redlint fix',

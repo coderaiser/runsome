829bfa24580d9c27a41ec367a1bca3f390a1fcf4
coderaiser <coderaiser@cloudcmd.io>
2026-06-02 12:37:23 +0000

    chore: lint

--- .madrun.js
+++ .madrun.js
@@ -5,6 +5,7 @@ export default {
     'coverage': () => 'c8 npm test',
     'lint': () => 'putout .',
     'fix:lint': () => run('lint', '--fix'),
+	'postfix:lint': () => 'redlint fix',
     'watcher': () => 'nodemon -w test -w lib --exec',
     'watch:test': async () => await run('watcher', `"${await run('test')}"`),
     'watch:lint': async () => await run('watcher', await run('lint')),
